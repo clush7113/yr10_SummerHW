0154106f856cca0f98886bf2e5dc0d611b9823bc
clush7113 <clush7113@sohamcollege.org.uk>
2014-06-06 14:22:00 +0100

    Update tables.py
    06/06/14

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,8 @@
-#paste code here
+def timeTable():
+    table=int(input("Which times table would you like to use?")) #gets times table used
+    numb=int(input("What number would you like to go up to?"))   #gets number to go up to
+    for x in range(1,numb+1):                                    #iterates over times table
+        print("{0} times {1} is {2}".format(table,x,table*x))    #prints times table and does math
+    
+timeTable()
+
